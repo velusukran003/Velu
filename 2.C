ad9a077786bfd3aa27f012132964ae1daaff7140
velusukran003 <30549639+velusukran003@users.noreply.github.com>
2017-08-05 16:40:45 +0530

    Create 2.C

---
+++ 2.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int c;
+    printf("Enter c: ");
+    scanf("%d", &c);
+    /* logic */
+    
+    if (c% 2 == 0) {
+    	printf("The given number is EVEN");
+    }
+    else {
+    	printf("The given number is ODD");
+    }
+    return 0;
+}
