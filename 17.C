ac0d088484854bf5c2a9f56b2cc3bd86518003f3
velusukran003 <30549639+velusukran003@users.noreply.github.com>
2017-08-12 16:27:31 +0530

    Create 17.C

---
+++ 17.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int number, originalNumber, remainder, result = 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+    return 0;
+}
+
+ 
+Output
+
+Enter a three digit integer: 371
+371 is an Armstrong number.
+Example #2: Check Armstrong Number of n digits
