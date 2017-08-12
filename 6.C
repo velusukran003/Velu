0c0c926c3aaf981d763fe0aacc98d18d4909b553
velusukran003 <30549639+velusukran003@users.noreply.github.com>
2017-08-12 16:16:08 +0530

    Update 6.C

--- 6.C
+++ 6.C
@@ -7,12 +7,12 @@ int main()
     printf("Enter a year: ");
     scanf("%d",&year);
 
-    if(year%4 == 0)
+    if(year%4 == 00)
     {
-        if( year%100 == 0)
+        if( year%100 ==00)
         {
             // year is divisible by 400, hence the year is a leap year
-            if ( year%400 == 0)
+            if ( year%400 ==00)
                 printf("%d is a leap year.", year);
             else
                 printf("%d is not a leap year.", year);
