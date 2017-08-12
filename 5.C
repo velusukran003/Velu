91cdb60828c6c2bc60f2ad679de3c413396cd89e
velusukran003 <30549639+velusukran003@users.noreply.github.com>
2017-08-12 16:10:57 +0530

    Update 5.C

--- 5.C
+++ 5.C
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    double n1, n2, n3;
+    double s1, s2, s3;
 
     printf("Enter three different numbers: ");
-    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    scanf("%lf %lf %lf", &s1, &s2, &s3);
 
-    if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
+    if( s1>=s2 && s1>=s3 )
+        printf("%.2f is the largest number.", s1);
 
-    if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
+    if( s2>=s1 && s2>=s3 )
+        printf("%.2f is the largest number.", s2);
 
-    if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
+    if( s3>=s1 && s3>=s2 )
+        printf("%.2f is the largest number.", s3);
 
     return 0;
 }
